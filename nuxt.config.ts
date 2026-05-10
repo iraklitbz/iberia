8e77a2188c2105f2b30f6a0dd14510fa521cd371
gabrieli <g.xokerashvili122018@gmail.com>
2026-05-10 13:11:28 +0200

    analitics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,6 +74,20 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700;800&family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Noto+Sans+Georgian:wght@300;400;500;600;700&family=Noto+Serif+Georgian:wght@400;500;600;700;800;900&display=swap',
         },
       ],
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-LKSKBE0TZE',
+          async: true,
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LKSKBE0TZE');
+          `,
+        },
+      ],
     },
     layoutTransition: { name: 'fade', mode: 'out-in' },
     pageTransition: { name: 'fade', mode: 'out-in' },
