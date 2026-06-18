5703c4856e9a6f127a928dcde81c42b815754036
gabrieli <g.xokerashvili122018@gmail.com>
2026-06-18 22:27:41 +0200

    Redirect non-subscribers to account page

--- app/middleware/subscriber.ts
+++ app/middleware/subscriber.ts
@@ -1,5 +1,6 @@
 export default defineNuxtRouteMiddleware(() => {
   const { isSubscriber, authReady } = useAuth()
+  const localePath = useLocalePath()
   if (!authReady.value) return
-  if (!isSubscriber.value) return navigateTo('/cuenta')
+  if (!isSubscriber.value) return navigateTo(localePath('/account'))
 })
